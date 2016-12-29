136990c4ee98c79b53a0ca1c61e299bb9ad58387
Andriy Letsyk <andriyl1993gmail.com>
2016-12-29 10:35:06 +0200

    Fix

--- error.py
+++ error.py
@@ -1,8 +1,8 @@
 
 class Error(Exception):
-    def __init__(self, msg, row=None, column=None, elem=None):
-        self.error_message = 'Error: index element - {0}, element - {1}.'.format(row, elem) + str(msg)
-        super(Exception, self).__init__('Error: index element - {0}, element - {1}. '.format(row, elem) + str(msg))
+    def __init__(self, msg, row=None, elem=None):
+        self.error_message = 'Error: element - {0}.'.format(elem) + str(msg)
+        super(Exception, self).__init__('Error: element - {0}. '.format(row, elem) + str(msg))
 
     def print_err(self, y, x):
         print "Row - %s. Column - %s. " % (y, x) + self.error_message
--- syntax_analizer.py
+++ syntax_analizer.py
@@ -1,6 +1,7 @@
 from tables import CONSTANTS, DELIMITERS, IDENTIFICATORS, KEYWORDS, WHITESPACES, SPECIFIC_SYMBOLS
 from error import Error
 from tree_view import show_tree
+import sys
 
 class SA(object):
     def __init__(self, codes):
@@ -21,9 +22,14 @@ class SA(object):
         except:
             return None
 
-    def print_tree_elem(self, index, name, add=True):
+    def add_error(self, name):
+        err = Error(name, self.curr_codes_elem.get('pos_y'), self.curr_code)
+        self.errors.append(err.print_err(self.curr_codes_elem.get('pos_y'), self.curr_codes_elem.get('pos_x')))
+        sys.exit("Error")
+
+    def print_tree_elem(self, index, name, code, lexem_value, add=True):
         if add:
-            print "-" * self.index + " " + name + ", " + str(index)
+            print "-" * self.index + " " + name + " | " + str(code) + " | " + lexem_value
         self.index = self.index + 1 if add else self.index - 1
 
     @show_tree('signal_program')
@@ -47,7 +53,7 @@ class SA(object):
                         self.curr_code = self._next()
                         if self.curr_code == DELIMITERS['.']:
                             return True
-        raise Error('program', None, None)
+        self.add_error('program')
 
     @show_tree('block')
     def _check_block(self):
@@ -57,7 +63,7 @@ class SA(object):
                 self.curr_code = self._next()
                 if KEYWORDS['END'] == self.curr_code:
                     return True
-        raise Error('block', None, None)
+        self.add_error('block')
 
     @show_tree('statements_list')
     def _check_statements_list(self):
@@ -72,7 +78,7 @@ class SA(object):
                     return True
         else:
             return True
-        raise Error('statements_list')
+        self.add_error('statements_list')
 
     @show_tree('declarations_list')
     def _check_declarations_list(self):
@@ -81,7 +87,7 @@ class SA(object):
                 return True
         else:
             return True
-        raise Error('declarations_list')
+        self.add_error('declarations_list')
 
     @show_tree('declaration')
     def _check_declaration(self):
@@ -94,7 +100,7 @@ class SA(object):
                                 return True
         else:
             return False
-        raise Error('declaration')
+        self.add_error('declaration')
 
     @show_tree('identifiers_list')
     def _check_identifiers_list(self):
@@ -105,7 +111,7 @@ class SA(object):
                     return True
         else:
             return True
-        raise Error("identifiers_list")
+        self.add_error('identifiers_list')
 
     @show_tree('attributes_list')
     def _check_attributes_list(self):
@@ -114,7 +120,7 @@ class SA(object):
                 return True
         else:
             return True
-        raise Error('attributes_list')
+        self.add_error('attributes_list')
 
     @show_tree('attribute')
     def _check_attribute(self):
@@ -131,7 +137,7 @@ class SA(object):
         self.curr_code = self._next()
         if self.curr_code in CONSTANTS.values():
             return True
-        raise Error('procedure_identifier')
+        self.add_error('procedure_identifier')
 
     @show_tree('variable_identifier')
     def _check_variable_identifier(self):
